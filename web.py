213ac865b747de44592ad52cf48d6303c18d52e9
liushuai <liushuai19970411@gmail.com>
2024-10-24 10:54:49 +0200

    web version implement add functions

--- web.py
+++ web.py
@@ -2,6 +2,13 @@ import streamlit as st
 import functions_new
 
 todos = functions_new.read_todos_doc()
+def add_todo():
+    todo = st.session_state['new_todo']
+    todo = todo + '\n'
+    todos.append(todo)
+    functions_new.write_todos_doc(todos)
+
+
 
 st.title('A fucking todo app')
 st.subheader('I said this is just a fucking todo app')
@@ -10,4 +17,7 @@ st.write('there is no fucking ending')
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label='Enter a shit here:', placeholder='Enter a shit here:')
+st.text_input(label='Enter a shit here:', placeholder='Enter a shit here:',
+              on_change=add_todo, key = 'new_todo')
+
+st.session_state
